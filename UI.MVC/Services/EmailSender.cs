6d89b7db225aea3ee43b1ac92961abbe903af17a
VincentB <vin.cent15.08.1998@gmail.com>
2019-05-24 16:32:28 +0200

    qsdgseg

--- UI.MVC/Services/EmailSender.cs
+++ UI.MVC/Services/EmailSender.cs
@@ -25,8 +25,6 @@ namespace UI.MVC.Services
             };
             msg.AddTo(new EmailAddress(email));
 
-            // Disable click tracking.
-            // See https://sendgrid.com/docs/User_Guide/Settings/tracking.html
             msg.SetClickTracking(false, false);
 
             return client.SendEmailAsync(msg);
