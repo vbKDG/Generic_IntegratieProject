ee01fb02bda9b984e3cc62606153c05447cd7c7b
Lukas De Graef <lukas.degraef@student.kdg.be>
2019-06-04 18:02:52 +0200

    eeee

---
+++ UI.MVC/Controllers/API/IdeationsController.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using BL;
+using Domain;
+using Microsoft.AspNetCore.Mvc;
+using UI.MVC.Models.Ideations;
+
+namespace UI.MVC.Controllers.api
+{
+    [ApiController]
+    public class IdeationsController : Controller
+    {
+        IIdeationManager mgr = new IdeationManager();
+        
+        [HttpGet("/api/Ideations/{id}")]
+        public IActionResult GetIdeations(int projectId)
+        {
+            List<Ideation> ideations = mgr.getIdeations(projectId).ToList();
+            Console.WriteLine(ideations.ToString());
+            List<IdeationRESTModel> newIdeations = new List<IdeationRESTModel>();
+            foreach (var ideation in ideations)
+            {
+                IdeationRESTModel model = new IdeationRESTModel()
+                {
+                    IdeationId = ideation.IdeationId,
+                    Questions = ideation.Questions,
+                    Ideas = ideation.Ideas
+                };
+                newIdeations.Add(model);
+            }
+            return Ok(newIdeations);
+        }
+    }
+}
---
+++ UI.MVC/Models/Ideations/IdeationRESTModel.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+using Domain;
+
+namespace UI.MVC.Models.Ideations
+{
+    public class IdeationRESTModel
+    {
+        public int IdeationId { get; set; }
+
+        public ICollection<IdeationQuestion> Questions { get; set; }
+        public ICollection<Idea> Ideas { get; set; }
+    }
+}
